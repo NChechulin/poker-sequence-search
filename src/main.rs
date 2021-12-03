b31611ac5f1a2e387e66a0eccd316d20fd701896
Nikolay Chechulin <nchechulin@protonmail.com>
2021-12-03 20:50:35 +0300

    implement z-algorithm on bytes

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,24 @@
 extern crate enum_primitive;
 
 mod card;
+mod zfinder;
+
+use zfinder::ZFinder;
 
 fn main() {
-    println!("Hello, world!");
+    let data = "0122333301222412412".as_bytes().into();
+    let pattern = "122".as_bytes().into();
+    let mut finder = ZFinder::new(&data, &pattern);
+
+    let all = finder.find_all();
+
+    match all {
+        Err(e) => println!("{}", e),
+        Ok(vec) => {
+            println!("All occurrences:");
+            for e in vec {
+                print!("{} ", e);
+            }
+        }
+    }
 }
---
+++ src/zfinder.rs
@@ -0,0 +1,89 @@
+pub struct ZFinder {
+    cursor: usize,
+    pattern_size: usize,
+    joined: Vec<u8>,
+    z_arr: Vec<usize>,
+}
+
+impl ZFinder {
+    pub fn new(data: &Vec<u8>, pattern: &Vec<u8>) -> ZFinder {
+        let mut joined = vec![];
+        joined.extend(pattern);
+        joined.push(u8::MAX);
+        joined.extend(data);
+
+        let mut result = ZFinder {
+            cursor: 0,
+            pattern_size: pattern.len(),
+            joined,
+            z_arr: vec![],
+        };
+        result.build_z_array();
+
+        result
+    }
+
+    fn compare_parts(&self, first: usize, second: usize) -> usize {
+        let mut cnt = 0;
+        let max = std::cmp::max(first, second);
+
+        while max + cnt < self.joined.len() && self.joined[first + cnt] == self.joined[second + cnt] {
+            cnt += 1;
+        }
+
+        cnt
+    }
+
+    fn build_z_array(&mut self) {
+        self.z_arr = vec![0; self.joined.len()];
+        let mut window_left = 0;
+        let mut window_right = 0;
+
+        for i in 1..self.joined.len() {
+            if i > window_right {
+                let cnt = self.compare_parts(i, 0);
+                self.z_arr[i] = cnt;
+                window_left = i;
+                window_right = i + cnt - 1;
+            } else {
+                if self.z_arr[i - window_left] < window_right - i + 1 {
+                    self.z_arr[i] = self.z_arr[i - window_left];
+                } else {
+                    let cnt = self.compare_parts(window_right + 1, window_right - window_left + 1);
+                    self.z_arr[i] = window_right - i + 1 + cnt;
+                    window_left = i;
+                    window_right = window_right + self.z_arr[i] - 1;
+                }
+            }
+        }
+    }
+
+    pub fn find(&mut self) -> Result<usize, &str> {
+        let start = std::cmp::max(self.cursor, self.pattern_size) + 1;
+
+        for i in start..self.joined.len() {
+            if self.z_arr[i] == self.pattern_size {
+                self.cursor = i;
+                return Ok(i - self.pattern_size - 1);
+            }
+        }
+
+        Err("No occurrences")
+    }
+
+    pub fn find_all(&mut self) -> Result<Vec<usize>, &str> {
+        let mut result = vec![];
+
+        loop {
+            match self.find() {
+                Ok(occurrence) => result.push(occurrence),
+                Err(_) => break,
+            }
+        }
+
+        return match result.is_empty() {
+            true => Err("No occurrences"),
+            false => Ok(result)
+        };
+    }
+}
